43e71b42e457cf7d391c71a30187ce7c582a652c
Jules Paris <jules.paris@wanadoo.fr>
2018-06-28 00:44:21 +0200

    test

--- server.js
+++ server.js
@@ -1,10 +1,4 @@
 const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const config = require('./server/config/DB');
-const todoRoutes = require('./server/routes/toDoRoutes');
 
 
 
